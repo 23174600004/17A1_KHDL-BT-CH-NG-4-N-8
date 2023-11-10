6eea6939fa5fe89cbdd2ce4f767657ecf68d1358
Nguyễn Thị Khánh Vy <ntkvy.dhkl17a1hn@sv.uneti.edu.vn>
2023-11-10 10:58:04 +0700

    init project

---
+++ Bài Tập/bt1.py
@@ -0,0 +1,38 @@
+#1.2 
+x=10
+y=5
+print("tổng",x+y)
+print("hiệu",x-y)
+print("tích",x*y)
+print("thương",x/y)
+    
+
+ #1.3
+ soluong=5
+dongia=25000
+thanhtien=soluong*dongia
+print('tên hàng :sữa hộp vinamilk')
+print('số lượng',soluong)
+print('đơn giá',dongia)
+print('thành tiền',thanhtien)
+   
+
+      
+
+#1.1 
+
+print(" **     **    ******    **        **       ******")
+print(" **     **    **        **        **       **  **")
+print(" *********    ******    **        **       **  **")
+print(" **     **    **        **        **       **  **")
+print(" **     **    ******    ******    ******   ******")
+
+
+
+
+
+
+
+
+
+
